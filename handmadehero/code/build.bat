07a8ff4b01665c2643a42a6962c31d883858587c
Guowei Lv <lvguowei1002@gmail.com>
2020-06-06 22:39:23 +0300

    some fixes

--- handmadehero/code/build.bat
+++ handmadehero/code/build.bat
@@ -1,7 +1,5 @@
 @echo off
-
 mkdir ..\..\build
-
 pushd ..\..\build
-
 cl -Zi ..\handmadehero\code\win32_handmadehero.cpp user32.lib gdi32.lib
+popd
--- handmadehero/misc/shell.bat
+++ handmadehero/misc/shell.bat
@@ -1,5 +1,5 @@
 @echo off
-
+subst w: c:\Users\lvguo\work\HandMadeHero
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-set path=c:\Users\lvguo\handmadehero\misc;%path%
+set path=c:\Users\lvguo\work\HandMadeHero\handmadehero\misc;%path%
